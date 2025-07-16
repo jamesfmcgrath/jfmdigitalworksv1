322ef6519c040a9afa7331a3237e6ed4823e9d84
James McGrath <james@mcgrathmedia.com>
2025-07-16 16:02:41 +0100

    Simple analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,7 +105,7 @@ export default function RootLayout({
             {children}
           </div>
         </Suspense>
-        
+
         <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
       </body>
     </html>
